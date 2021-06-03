94ce97c9477c2cda1d96d9af046077322b359117
Artem Vetrov <artem.vetrov@republic.co>
2021-06-04 02:09:25 +0300

    Add arguments validation

--- lib/image_downloader.rb
+++ lib/image_downloader.rb
@@ -4,9 +4,12 @@ require_relative 'image_downloader/downloader'
 module ImageDownloader
   class << self
     def call(args)
-      file_name, folder_path = args
+      file_name, directory_path = args
+      if file_name.nil? || directory_path.nil?
+        return puts 'File and folder name are required!'
+      end
       urls = UrlExtractor.new(file_name).call
-      Downloader.new(urls, folder_path).call
+      Downloader.new(urls, directory_path).call
     end
   end
 end
